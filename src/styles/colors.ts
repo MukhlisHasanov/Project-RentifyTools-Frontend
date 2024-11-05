9cd641cbbbd7f066945762c73776678c1d819cda
solov81yova <solov81yova@gmail.com>
2024-11-05 12:39:22 +0100

    Initialize Vite project with Redux setup

---
+++ src/styles/colors.ts
@@ -0,0 +1,30 @@
+export interface Colors {
+  PRIMARY: string;
+  TEXT_BLUE: string;
+  PRIMARY_BLUE: string;
+  SHADOW_BLUE: string;
+  BACKGROUND_WHITE: string;
+  ERROR: string;
+  DISABLED: string;
+  GREY: string;
+  WHITE: string;
+  CARD: string;
+  HEADER: string;
+  TRANSPARENT: string;
+  SEARCH: string;
+}
+export const colors: Colors = {
+  PRIMARY: "rgb(26, 35, 53)",
+  TEXT_BLUE: "#0f1380",
+  PRIMARY_BLUE: "#1f27f5",
+  SHADOW_BLUE: "#888abf",
+  BACKGROUND_WHITE: "#ffffff",
+  ERROR: "rgb(206, 44, 44)",
+  DISABLED: "rgb(128, 128, 128)",
+  GREY: "rgb(186, 186, 186)",
+  WHITE: "#FFFFFF",
+  CARD: "linear-gradient(133.66deg, rgba(47, 72, 111, 0.62) 5.78%, rgba(11, 27, 52, 0.62) 96.58%)",
+  HEADER: "linear-gradient(0deg, #122d4d80, #122d4d80), linear-gradient(0deg, #ffffff1a, #ffffff1a)",
+  TRANSPARENT: "transparent",
+  SEARCH: "rgba(54, 120, 180, 1)",
+}
