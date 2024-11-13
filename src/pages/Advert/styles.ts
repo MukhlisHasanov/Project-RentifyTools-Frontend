c6aaf09f13e402e4f0a3b99daa38ecd3fdd2d905
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-11-13 20:46:49 +0400

    write advert page draft vers

---
+++ src/pages/Advert/styles.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const PageWrapper = styled.div`
+`
+
+export const PhotoFrame = styled.div``
+
+export const DescriptionFrame = styled.div``
+
+export const ToolInfo = styled.div``
+
+export const UserInfo = styled.div``
+
+export const PhotoWrapper = styled.div``
