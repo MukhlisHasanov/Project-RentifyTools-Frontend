52ec47fc50f544a1d2312423bffe9cdf8876a533
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-11-14 12:00:21 +0400

    setup redax

--- src/pages/Advert/Advert.tsx
+++ src/pages/Advert/Advert.tsx
@@ -1,30 +1,30 @@
-import { PageWrapper } from "./styles";
-import Button from "components/Button/Button";
-import Card from "components/Card/Card"
-import User from "components/User/User"
+// import { PageWrapper } from "./styles";
+// import Button from "components/Button/Button";
+// import Card from "components/Card/Card"
+// import User from "components/User/User"
 
-function Advert() {
+// function Advert() {
 
-    return <PageWrapper>
-        <PhotoWrapper>
-        <Button name="<"/>
-        <PhotoFrame>
-        </PhotoFrame>
-        <Button name=">"/>
-        </PhotoWrapper>
-        <DescriptionFrame>
-            <ToolInfo>
-                <Card />
+//     return <PageWrapper>
+//         <PhotoWrapper>
+//         <Button name="<"/>
+//         <PhotoFrame>
+//         </PhotoFrame>
+//         <Button name=">"/>
+//         </PhotoWrapper>
+//         <DescriptionFrame>
+//             <ToolInfo>
+//                 <Card />
 
-            </ToolInfo>
-            <UserInfo>
-                <User />
-                <Button name="Send message"/>
-                <Button name="Show phone"/>
-            </UserInfo>
-        </DescriptionFrame>
+//             </ToolInfo>
+//             <UserInfo>
+//                 <User />
+//                 <Button name="Send message"/>
+//                 <Button name="Show phone"/>
+//             </UserInfo>
+//         </DescriptionFrame>
 
-    </PageWrapper>
-}
+//     </PageWrapper>
+// }
 
-export default Advert;
+// export default Advert;
--- src/pages/Advert/styles.ts
+++ src/pages/Advert/styles.ts
@@ -1,14 +1,28 @@
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
 
-export const PageWrapper = styled.div`
-`
+// export const PageWrapper = styled.div`
+// display: flex;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center;
+// flex: 1;
+// gap: 10px;
+// `
+// export const PhotoWrapper = styled.div`
+// display: flex;
+// flex-direction: row;
 
-export const PhotoFrame = styled.div``
+// `
 
-export const DescriptionFrame = styled.div``
+// export const PhotoFrame = styled.div`
 
-export const ToolInfo = styled.div``
+// `
 
-export const UserInfo = styled.div``
+// export const DescriptionFrame = styled.div`
 
-export const PhotoWrapper = styled.div``
+// `
+
+// export const ToolInfo = styled.div``
+
+// export const UserInfo = styled.div``
+
