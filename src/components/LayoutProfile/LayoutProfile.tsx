88a88bf87327f264bacbab24de447d97f60a774a
ilofrudd <boyko5000@gmail.com>
2024-11-14 20:48:20 +0100

    LayoutProfile

--- src/components/LayoutProfile/LayoutProfile.tsx
+++ src/components/LayoutProfile/LayoutProfile.tsx
@@ -1,109 +1,51 @@
-import { useNavigate } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import { v4 } from "uuid"
-import { useState, ChangeEvent } from "react"
 
 import { TOOLS_APP_ROUTES } from "constants/routes"
-import Input from "components/Input/Input"
-import Button from "components/Button/Button"
-
-import { LayoutProps } from "./types"
 import {
-  LayoutWrapper,
-  AppHeader,
-  AppTitle,
-  HeaderLink,
-  HeaderNav,
-  AppMain,
-  AppFooter,
-  FooterNav,
-  FooterLink,
-  SearchContainer,
+  ProfileWrapper,
+  Sidebar,
+  SidebarNav,
+  SidebarLink,
+  Content,
+  UserProfile,
+  UserPhoto,
+  UserName,
 } from "./styles"
+import { UserImg } from "assets"
 
-function LayoutProfile({ children }: LayoutProps) {
-  const [toolName, setToolName] = useState<string>("")
+function LayoutProfile() {
   const navigate = useNavigate()
 
-  const getToolData = () => {
-    if (!toolName.trim()) {
-      alert("Please enter a tool`s title")
-      return
-    }
-    // dispatch(
-    //   weatherSliceAction.getWeatherData({ cityName: cityName, appKey }),
-    // )
-  }
-  const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log("Input title:", event.target.value)
-    // dispatch(weatherSliceAction.getCityName(event.target.value))
-    setToolName(event.target.value)
-  }
-  const goToHomePage = () => {
-    navigate(TOOLS_APP_ROUTES.HOME)
-  }
-
-  const appLinksHeader = {
-    [TOOLS_APP_ROUTES.HOME]: "Home",
-    [TOOLS_APP_ROUTES.ADD_ADVERTS]: "Add Adverts",
+  const profileLinks = {
     [TOOLS_APP_ROUTES.PROFILE]: "Profile",
+    [TOOLS_APP_ROUTES.MESSAGES]: "Messages",
+    [TOOLS_APP_ROUTES.MY_ADVERTS]: "My Adverts",
+    [TOOLS_APP_ROUTES.FAVORITES]: "Favorites",
+    [TOOLS_APP_ROUTES.RENTED_TOOLS]: "Rented Tools",
   }
 
-  const appLinksFooter = {
-    [TOOLS_APP_ROUTES.HELP]: "Help",
-    [TOOLS_APP_ROUTES.ADVERTISING]: "Advertising",
-    [TOOLS_APP_ROUTES.ABOUT_US]: "About us",
-    [TOOLS_APP_ROUTES.CONTACTS]: "Contacts",
-    [TOOLS_APP_ROUTES.PRIVACY_POLICY]: "Privacy Policy",
-    [TOOLS_APP_ROUTES.CONDITIONS]: "Conditions of use",
-    [TOOLS_APP_ROUTES.IMPRINT]: "Imprint",
-    [TOOLS_APP_ROUTES.SOCIAL_MEDIA]: "Social media",
-  }
-  const headerLinks = Object.keys(appLinksHeader).map((link: string) => {
+  const sidebarLinks = Object.keys(profileLinks).map(link => {
     return (
-      <HeaderLink
-        key={v4()}
-        style={({ isActive }) => ({
-          //   fontWeight: isActive ? "bold" : "normal",
-          textDecoration: isActive ? "underline" : "none",
-        })}
-        to={link}
-      >
-        {appLinksHeader[link as keyof typeof appLinksHeader]}
-      </HeaderLink>
-    )
-  })
-
-  const footerLinks = Object.keys(appLinksFooter).map((link: string) => {
-    return (
-      <FooterLink key={v4()} to={link}>
-        {appLinksFooter[link as keyof typeof appLinksFooter]}
-      </FooterLink>
+      <SidebarLink key={v4()} to={link}>
+        {profileLinks[link as keyof typeof profileLinks]}
+      </SidebarLink>
     )
   })
 
   return (
-    <LayoutWrapper>
-      <AppHeader>
-        <AppTitle onClick={goToHomePage}>RENTIFY TOOLS</AppTitle>
-        <SearchContainer>
-          <Input id="search-tool" name="tool" placeholder="Search tool" onChange={onChangeValue}/>
-          <Button
-            type="button"
-            name="Search"
-            isSmallButton
-            onClick={getToolData}
-          />
-        </SearchContainer>
-        <HeaderNav>
-          <HeaderNav>{headerLinks}</HeaderNav>
-        </HeaderNav>
-      </AppHeader>
-      <AppMain>{children}</AppMain>
-      <AppFooter>
-        <FooterNav>{footerLinks}</FooterNav>
-      </AppFooter>
-    </LayoutWrapper>
+    <ProfileWrapper>
+      <Sidebar>
+        <UserProfile>
+          <UserPhoto src={UserImg} alt="User Photo" />
+          <UserName>John Doe</UserName>
+        </UserProfile>
+        <SidebarNav>{sidebarLinks}</SidebarNav>
+      </Sidebar>
+      <Content>
+        <Outlet />
+      </Content>
+    </ProfileWrapper>
   )
 }
-
 export default LayoutProfile
