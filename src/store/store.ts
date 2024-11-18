9a1fd92cf913853d0637778da0a39fc04988a7c0
AndreyHein <soly.a@web.de>
2024-11-18 19:34:00 +0100

    add singUpSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import type { Action, ThunkAction } from '@reduxjs/toolkit'
 import { combineSlices, configureStore } from '@reduxjs/toolkit'
+
+import { registerUser } from 'store/redux/signUpSlice/signUpSlice' 
 // import { userSlice } from 'store/redux/userSlice/userSlice'
 
-const rootReducer = combineSlices()
+const rootReducer = combineSlices(registerUser)
 
 export type RootState = ReturnType<typeof rootReducer>
 
