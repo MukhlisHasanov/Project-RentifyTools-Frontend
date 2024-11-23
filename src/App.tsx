1fb04900487b24f576f8ea5dce45153170700b2c
ilofrudd <boyko5000@gmail.com>
2024-11-23 20:53:24 +0100

    Advert rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Layout from "components/Layout/Layout"
-import LayoutProfile from "components/LayoutProfile/LayoutProfile"
-import Home from "pages/Home/Home"
-import MyAdvert from "pages/MyAdvert/MyAdvert"
-import SignUpForm from "pages/Login/Login"
-import { TOOLS_APP_ROUTES } from "constants/routes"
-import Profile from "pages/Profile/Profile"
-import NewAdvertForm from "components/NewAdvertForm/NewAdvertForm"
+import Layout from 'components/Layout/Layout'
+import LayoutProfile from 'components/LayoutProfile/LayoutProfile'
+import Home from 'pages/Home/Home'
+import MyAdvert from 'pages/MyAdvert/MyAdvert'
+import SignUpForm from 'pages/Login/Login'
+import { TOOLS_APP_ROUTES } from 'constants/routes'
+import Advert from 'pages/Profile/Profile'
+import NewAdvertForm from 'components/NewAdvertForm/NewAdvertForm'
 import ChangeAdvert from 'pages/ChangeAdvert/ChangeAdvert'
-import AddAdvert from "pages/AddAdvert/AddAdvert"
-import SignInForm from "components/SignInForm/SignInForm"
-import Products from 'pages/Advert/Advert'
+import AddAdvert from 'pages/AddAdvert/AddAdvert'
+import SignInForm from 'components/SignInForm/SignInForm'
+import Advert from 'pages/Advert/Advert'
 import ChangeAdvertForm from 'components/ChangeAdvertForm/ChangeAdvertForm'
 import HomePlug from 'pages/HomePlug/HomePlug'
 
@@ -24,16 +24,18 @@ function App() {
           <Route path={TOOLS_APP_ROUTES.ADD_ADVERTS} element={<AddAdvert />} />
           <Route path={TOOLS_APP_ROUTES.LOGIN} element={<SignUpForm />} />
           <Route path={TOOLS_APP_ROUTES.PROFILE} element={<LayoutProfile />}>
-            <Route path={TOOLS_APP_ROUTES.PROFILE} element={<Profile />} />
+            <Route path={TOOLS_APP_ROUTES.PROFILE} element={<Advert />} />
             <Route path="/profile/add-adverts" element={<NewAdvertForm />} />
             <Route path="/profile/messages" element={<HomePlug />} />
             <Route path="/profile/my-adverts" element={<MyAdvert />} />
-            <Route path="/profile/change-adverts" element={<ChangeAdvertForm />} />
+            <Route
+              path="/profile/change-adverts"
+              element={<ChangeAdvertForm />}
+            />
             <Route path="/profile/favourites" element={<HomePlug />} />
             <Route path="/profile/rented-tools" element={<HomePlug />} />
-            
           </Route>
-          <Route path="/products" element={<Products />} />
+          <Route path="/products" element={<Advert />} />
         </Route>
       </Routes>
     </BrowserRouter>
