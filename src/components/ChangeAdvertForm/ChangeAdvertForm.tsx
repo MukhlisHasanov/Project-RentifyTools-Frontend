8ed615003ea90078156404857a6dc1e8478ab6d8
ilofrudd <boyko5000@gmail.com>
2024-12-06 19:40:20 +0100

    .

--- src/components/ChangeAdvertForm/ChangeAdvertForm.tsx
+++ src/components/ChangeAdvertForm/ChangeAdvertForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
