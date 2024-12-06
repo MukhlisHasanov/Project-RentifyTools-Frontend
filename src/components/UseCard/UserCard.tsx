83544aa5553a2a4e8931cfcd71baf4a30079f87d
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-12-06 20:21:47 +0100

    061224 Update userSlice, Update UserCard

--- src/components/UseCard/UserCard.tsx
+++ src/components/UseCard/UserCard.tsx
@@ -8,9 +8,30 @@ import { UserProps } from './types'
 import { UserName } from 'components/LayoutProfile/styles'
 import { UserInfo } from './styles'
 import { colors } from 'styles/colors'
+import { userSliceAction } from 'store/redux/userSlice/userSlice'
+import { useDispatch } from 'react-redux'
+import { signInOutSliceAction } from 'store/redux/signInSlice/signInOutSlice'
+import { useAppDispatch } from 'store/hooks'
+
 
 function UserCard({ userData, error }: UserProps) {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch() 
+
+  const handleDelete = async () => {
+    if (window.confirm('Bist du sicher, dass du den Benutzer löschen möchtest?')) {
+      try {
+        const result = await dispatch(userSliceAction.deleteUser()) 
+        if (userSliceAction.deleteUser.fulfilled.match(result)) {
+          console.log('Benutzer erfolgreich gelöscht:')
+        } else {
+          console.error('Fehler beim Löschen:', result.payload || result.error)
+        }
+      } catch (error) {
+        console.error('Ein unerwarteter Fehler ist aufgetreten:', error)
+      }
+    }
+  }
 
   return (
     <UserContainer>
@@ -32,8 +53,8 @@ function UserCard({ userData, error }: UserProps) {
           <EditIcon />
         </IconButton>
         <IconButton
-          onClick={() => console.log('Delete user')}
-          sx={{ color: colors.BUTTON }}
+           onClick={() => handleDelete()}  
+           sx={{ color: colors.BUTTON }}
         >
           <DeleteIcon />
         </IconButton>
