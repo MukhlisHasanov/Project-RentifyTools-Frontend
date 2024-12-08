20bd584ce41e852af8af7e40b838ea98c707f515
AndreyHein <soly.a@web.de>
2024-12-08 13:46:01 +0100

    add Category

---
+++ src/store/redux/CategorySlice/categorySlice.ts
@@ -0,0 +1,51 @@
+import { createAppSlice } from 'store/createAppSlice'
+import { CategoriesInitialState, Category } from './types'
+
+const categoryDataInitialState: CategoriesInitialState = {
+  categories: [],
+  isLoading: false,
+  error: undefined,
+}
+
+export const categorySlice = createAppSlice({
+  name: 'CATEGORIES_DATA',
+  initialState: categoryDataInitialState,
+  reducers: create => ({
+    fetchCategories: create.asyncThunk(
+      async (_, { rejectWithValue }) => {
+        const response = await fetch('/api/categories')
+        const result = await response.json()
+        console.log("Result from API:", result);
+
+        if (!response.ok) {
+          return rejectWithValue(result.message || 'Error fetching categories')
+        }
+        return result as Category[]
+      },
+      {
+        pending: (state: CategoriesInitialState) => {
+          state.isLoading = true
+          state.error = undefined
+        },
+        fulfilled: (state: CategoriesInitialState, action) => {
+          state.isLoading = false
+          state.categories = action.payload.map((category: Category) => ({
+            ...category,
+            image: `data:image/jpeg;base64,${category.image}`,
+          }))
+          console.log("State:", state.categories);
+        },
+        rejected: (state: CategoriesInitialState, action) => {
+          state.isLoading = false
+          state.error = action.payload as string
+        },
+      },
+    ),
+  }),
+  selectors: {
+    categories_data: (state: CategoriesInitialState) => state,
+  },
+})
+
+export const categorySliceAction = categorySlice.actions
+export const categorySliceSelectors = categorySlice.selectors
