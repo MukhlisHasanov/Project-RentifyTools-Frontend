1de8003d31f50f1e2763ee576ea98ea15a739a53
solov81yova <solov81yova@gmail.com>
2024-12-12 22:09:19 +0100

    update NewAdvertForm with success message and button Search for click Enter

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,42 +1,23 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { useAppDispatch, useAppSelector } from 'store/hooks'
-import {
-  userSliceAction,
-  userSliceSelectors,
-} from 'store/redux/userSlice/userSlice'
 
 import SignUpForm from 'components/SignUpForm/SignUpForm'
 import SignInForm from 'components/SignInForm/SignInForm'
 
-import { PageWrapper, SuccessMessage } from './styles'
+import { PageWrapper } from './styles'
 
 function Login() {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
-  const { userObj, isLoading, error } = useAppSelector(
-    userSliceSelectors.user_data,
-  )
   const isRegistered = Boolean(localStorage.getItem('accessToken'))
 
   const [isSignInMode, setIsSignInMode] = useState<boolean>(true)
-  const [showSuccessMessage, setShowSuccessMessage] = useState<boolean>(false)
 
   const onSignUpClick = () => setIsSignInMode(false)
   const onSignInClick = () => setIsSignInMode(true)
 
   const registrationSuccess = () => {
     setIsSignInMode(true)
-    setShowSuccessMessage(true)
-    setTimeout(() => setShowSuccessMessage(false), 3000)
   }
   return (
     <PageWrapper>
-      {showSuccessMessage && (
-        <SuccessMessage>Registration successful! Please sign in</SuccessMessage>
-      )}
-
       {isSignInMode || isRegistered ? (
         <SignInForm onSwitchToSignUp={onSignUpClick} />
       ) : (
