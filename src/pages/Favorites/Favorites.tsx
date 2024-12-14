dbc5eea334d735a9ab233a0bf18284d3edbf491f
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-12-14 18:13:18 +0400

    refactor favorites page

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,43 +1,35 @@
-import { useAppDispatch, useAppSelector } from 'store/hooks'
+import { useAppSelector } from 'store/hooks'
 import { PageWrapper, CardsContainer, TextContainer } from './styles'
 import ToolCard from 'components/ToolCard/ToolCard'
-import {
-  toolSliceAction,
-  toolSliceSelectors,
-} from 'store/redux/ToolSlice/toolSlice'
-import { TOOLS_APP_ROUTES } from 'constants/routes'
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import { AsyncThunkAction } from '@reduxjs/toolkit'
-import { ToolUserResponseDto } from 'store/redux/ToolSlice/types'
+import { toolSliceSelectors } from 'store/redux/ToolSlice/toolSlice'
 
 function Favorites() {
   const { favCards } = useAppSelector(toolSliceSelectors.tools_data)
 
-  if (!favCards) {
-    return <TextContainer>No favorites yet</TextContainer>
+  if (!favCards || favCards.length === 0) {
+    return (
+      <PageWrapper>
+        <TextContainer>No favorite tools added yet.</TextContainer>
+      </PageWrapper>
+    )
   }
 
   return (
     <PageWrapper>
-      {favCards.length === 0 ? (
-        <TextContainer>No favorite tools added yet.</TextContainer>
-      ) : (
-        <CardsContainer>
-          {favCards.map(tool => (
-            <ToolCard
-              id={tool.id}
-              key={tool.id}
-              imageUrls={tool.imageUrls}
-              title={tool.title}
-              price={tool.price}
-              status={tool.status}
-              description={tool.description}
-              onAddToCard={() => {}}
-            />
-          ))}
-        </CardsContainer>
-      )}
+      <CardsContainer>
+        {favCards.map(tool => (
+          <ToolCard
+            id={tool.id}
+            key={tool.id}
+            imageUrls={tool.imageUrls}
+            title={tool.title}
+            price={tool.price}
+            status={tool.status}
+            description={tool.description}
+            onAddToCard={() => {}}
+          />
+        ))}
+      </CardsContainer>
     </PageWrapper>
   )
 }
